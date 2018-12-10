d20c793b08c660a6ec628a07a144bbff7048ad6c
16301155 <37803218+16301155@users.noreply.github.com>
2018-12-10 16:45:33 +0800

    上传项目：作业3

---
+++ app/src/main/java/com/example/myungger/myapplication/activity/EmailActivity.java
@@ -0,0 +1,10 @@
+package com.example.myungger.myapplication.activity;
+
+import android.os.Bundle;
+
+public class EmailActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
